9210a7ca4ff88cdb8e5a9f5c73301ca12f68dfab
sisosyunya <syunyasisotto@gmail.com>
2024-02-18 06:49:54 +0900

    unansweredpage

--- lib/pages/answered_question_page.dart
+++ lib/pages/answered_question_page.dart
@@ -9,10 +9,47 @@ class AnsweredQuestionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(faq.question)),
+      appBar: AppBar(
+        title: const Text(
+          '回答がある質問',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true, // タイトルを中央に配置
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(faq.answer!),
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Card(
+            elevation: 4.0, // カードの影を追加
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min, // コンテンツに合わせてカードのサイズを最小限にする
+                crossAxisAlignment: CrossAxisAlignment.center, // 要素を中央揃えにする
+                children: [
+                  Text(
+                    '質問：${faq.question}',
+                    textAlign: TextAlign.center, // テキストを中央揃えにする
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    '回答：${faq.answer ?? ""}',
+                    style: const TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center, // テキストを中央揃えにする
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/pages/un_answered_question.dart
+++ lib/pages/un_answered_question.dart
@@ -11,21 +11,64 @@ class UnAnsweredQuestionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Unanswered Questions'),
+        title: const Text(
+          '未回答の質問',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true, // タイトルを中央に配置
       ),
-      body: Column(
-        children: [
-          Text(faq.question),
-          const SizedBox(height: 8),
-          Text(faq.keyword),
-          const SizedBox(height: 8),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => AddAnswerPage(questionId: faq.id)));
-              },
-              child: const Text('回答する'))
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Card(
+            elevation: 4.0, // カードの影を追加
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min, // コンテンツに合わせてカードのサイズを最小限にする
+                crossAxisAlignment: CrossAxisAlignment.center, // 要素を中央揃えにする
+                children: [
+                  Text(
+                    '質問：${faq.question}',
+                    textAlign: TextAlign.center, // テキストを中央揃えにする
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'もし回答できる場合は、ぜひお願いします！',
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center, // テキストを中央揃えにする
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) =>
+                              AddAnswerPage(questionId: faq.id)));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 10), // ボタンのパディングを調整
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('回答する',
+                          style: TextStyle(
+                            fontSize: 20,
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
