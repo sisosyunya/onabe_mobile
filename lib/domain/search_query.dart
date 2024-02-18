03fbe573e0352a08314b876891cbb20335ec0c23
sisosyunya <syunyasisotto@gmail.com>
2024-02-18 12:33:51 +0900

    apikey削除

--- lib/domain/search_query.dart
+++ lib/domain/search_query.dart
@@ -1,11 +1,5 @@
-import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final _productsSearcher = HitsSearcher(
-    applicationID: 'YN6GNHV33F',
-    apiKey: '70439f32ac90dd19d92102206459997f',
-    indexName: 'faq');
 
 class SearchAppBar extends HookConsumerWidget implements PreferredSizeWidget {
   const SearchAppBar({Key? key}) : super(key: key);
