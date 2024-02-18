825014c8197638f1bed3b6b8c4b6a38046babf51
sisosyunya <syunyasisotto@gmail.com>
2024-02-18 12:30:23 +0900

    回答済みと未回答の表記わけ

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -4,8 +4,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:onabe_mobile/domain/faq_converter.dart';
 import 'package:onabe_mobile/domain/models/answered_question.dart';
 import 'package:onabe_mobile/domain/models/un_answered_question.dart';
+import 'package:onabe_mobile/domain/search_query.dart';
 import 'package:onabe_mobile/pages/add_answer_page.dart';
-import 'package:onabe_mobile/pages/components/search_appbar.dart';
 import 'package:onabe_mobile/providers.dart';
 
 class TopPage extends ConsumerWidget {
@@ -16,46 +16,61 @@ class TopPage extends ConsumerWidget {
     // final textInput = ref.watch(textInputProvider);
     final faqList = ref.watch(faqProvider);
     return Scaffold(
-        appBar: SearchAppBar(),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: faqList.when(
-              loading: () => const CircularProgressIndicator(),
-              //　エラー内容を表示する
-              error: (error, stackTrace) => Text(error.toString()),
-              data: (data) => ListView.builder(
-                    itemCount: data.length,
-                    itemBuilder: (context, index) {
-                      final faq = data[index];
-                      final specificFAQ = convertFAQToSpecificType(faq);
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          leading: SvgPicture.asset(
-                            'assets/images/wanko.svg',
-                            width: 24,
-                            height: 24,
-                          ),
-                          title: Text(specificFAQ.question.toString()),
-                          onTap: () {
-                            if (specificFAQ is AnsweredQuestion) {
-                              showDialog(
-                                  context: context,
-                                  builder: (context) =>
-                                      AnsweredQuestionDialog(faq: specificFAQ));
-                            } else if (specificFAQ is UnansweredQuestion) {
-                              showDialog(
-                                  context: context,
-                                  builder: (context) =>
-                                      UnAnsweredQuestionDialog(
-                                          faq: specificFAQ));
-                            }
-                          },
-                        ),
-                      );
+      appBar: const SearchAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: faqList.when(
+          loading: () => const CircularProgressIndicator(),
+          //　エラー内容を表示する
+          error: (error, stackTrace) => Text(error.toString()),
+          data: (data) => ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              final faq = data[index];
+              final specificFAQ = convertFAQToSpecificType(faq);
+              if (specificFAQ is AnsweredQuestion) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: SvgPicture.asset(
+                      'assets/images/wanko.svg',
+                      width: 24,
+                      height: 24,
+                    ),
+                    title: Text("[回答済み]${specificFAQ.question}"),
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) =>
+                              AnsweredQuestionDialog(faq: specificFAQ));
                     },
-                  )),
-        ));
+                  ),
+                );
+              } else if (specificFAQ is UnansweredQuestion) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: SvgPicture.asset(
+                      'assets/images/wanko.svg',
+                      width: 24,
+                      height: 24,
+                    ),
+                    title: Text("[未回答]${specificFAQ.question}"),
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) =>
+                              UnAnsweredQuestionDialog(faq: specificFAQ));
+                    },
+                  ),
+                );
+              }
+              return null;
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
 
